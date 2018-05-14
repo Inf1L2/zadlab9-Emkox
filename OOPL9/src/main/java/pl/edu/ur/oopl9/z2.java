41b0747bca2ad0bc7148fbf991efba5d46726c87
Emek <Emek@Emek-PC>
2018-05-14 16:01:24 +0200

    Klasa complex dodana w folderze zadania 1

--- OOPL9/src/main/java/pl/edu/ur/oopl9/z2.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/z2.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pl.edu.ur.oopl9;
 
 import java.util.Iterator;
 import java.util.Random;
 import java.util.TreeSet;
 
-/**
- *
- * @author Mati
- */
 public class z2 {
     public static void main(String[] args){
     Random losujLiczby = new Random();
--- OOPL9/src/main/java/pl/edu/ur/oopl9/z4.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/z4.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pl.edu.ur.oopl9;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
-/**
- *
- * @author Mati
- */
 public class z4 {
        public static void main(String[] args){
        HashMap<String,String> hm = new HashMap<>();
